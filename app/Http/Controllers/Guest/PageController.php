960e83c9e70b59829de8b3061233e0b9c29281ef
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-05-12 15:50:55 +0200

    creating update

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -11,6 +11,8 @@ class PageController extends Controller
     public function index() {
 
         $trains = Train::all();
+
+        dd($trains);
         return view('home', compact('trains'));
 
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,11 @@
 @section('title', 'train list')
 
 @section('content')
-    
+    <div>
+        @foreach ($trains as $train)
+           
+            {{$train}}
+            
+        @endforeach
+    </div>
 @endsection
