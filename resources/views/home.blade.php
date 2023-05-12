43aee0d0d1cbf3ca1b76f7f82e7e94cbbd32e897
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-05-12 17:21:52 +0200

    creating layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,33 @@
 
 @section('content')
     <div class="container">
-        @foreach ($trains as $train)
-           
-            {{$train}}
-            
-        @endforeach
+        <div class="row">
+
+            <div class="wrapper d-flex flex-wrap justify-content-between">
+    
+                @foreach ($trains as $train)
+
+                    @foreach ($train as $item)
+                        
+                    @dump($item)
+                    <div class="card col-3 my-4 text-white" style="width: 18rem;">
+                        <div class="card-body">
+                            <h4 class="card-title text-center">azienda: {{$item}}</h4>
+                            <h5 class="card-text">Stazione di Partenza: {{$item}}</h5>
+                            <h5 class="card-text">Stazione di Arrivo: {{$item}}</h5>
+                            <p class="card-text myText">Orario di partenza: {{$item}}</p>
+                            <p class="card-text myText">Orario di arrivo: {{$item}}</p>
+                            <p class="card-text myText">Numero Carrozze: {{$item}}</p>
+                            <p class="card-text myText">In Orario: {{$item}}</p>
+                            <p class="card-text myText">Cancellato: {{$item}}</p>
+                            <p class="card-text myText">Giorno Partenza: {{$item}}</p>
+                        </div>
+                    </div>
+                    @endforeach
+                    
+    
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
